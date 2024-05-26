6e49b0e6fae7dbee229c5b0ca3f2389133b560a6
Rana Osama <66523119+RanaUsama20@users.noreply.github.com>
2024-05-27 00:45:26 +0300

    edited Ui

--- lib/TaskList/taskWidget.dart
+++ lib/TaskList/taskWidget.dart
@@ -19,105 +19,107 @@ class _TaskWidgetState extends State<TaskWidget> {
   Widget build(BuildContext context) {
     var provider = Provider.of<AppConfigProvider>(context);
     // TODO: implement build
-    return Slidable(
-      startActionPane: ActionPane(
-        extentRatio: 0.25,
-        motion: const ScrollMotion(),
-        children:  [
-          SlidableAction(
-            borderRadius: BorderRadius.circular(15),
-            onPressed: (context){
+    return Container(
+      margin: EdgeInsets.all(18),
+      child: Slidable(
+        startActionPane: ActionPane(
+          extentRatio: 0.25,
+          motion: const ScrollMotion(),
+          children:  [
+            SlidableAction(
+              borderRadius: BorderRadius.circular(15),
+              onPressed: (context){
 
-            },
-            backgroundColor: MyTheme.redColor,
-            foregroundColor: MyTheme.whiteColor,
-            icon: Icons.delete,
-            label: 'Delete',
-          ),
-        ],
-      ),
-      child: InkWell(
-        onTap: (){
-          Navigator.of(context).pushNamed(EditTask.routeName);
-        },
-        child: Container(
-          padding: EdgeInsets.all(15),
-          margin: EdgeInsets.all(18),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            color: provider.isDarkMode()?
-                MyTheme.darkGreyColor :
-                MyTheme.whiteColor
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                color:  isDone?
-                    MyTheme.limeColor :
-                    MyTheme.primaryColor,
-                height: 80,
-                width: 4,
-              ),
-              Expanded(
-                  child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                    AppLocalizations.of(context)!.task_title,
-                    style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                      color: isDone?
-                      MyTheme.limeColor
-                        :
-                      MyTheme.primaryColor
-                    ),),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      AppLocalizations.of(context)!.task_details,
-                    style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                        color: provider.isDarkMode()?
-                        MyTheme.whiteColor :
-                        MyTheme.blackColor)),
-                  ),
-                ],
-              )),
-              InkWell(
-                onTap: (){
-                  isDone = true;
-                  setState(() {
-
-                  });
-                },
-                child: isDone?
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text('${AppLocalizations.of(context)!.done}!',
-                    style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                        color: MyTheme.limeColor
-                    ),),
-                )
-                    :
+              },
+              backgroundColor: MyTheme.redColor,
+              foregroundColor: MyTheme.whiteColor,
+              icon: Icons.delete,
+              label: 'Delete',
+            ),
+          ],
+        ),
+        child: InkWell(
+          onTap: (){
+            Navigator.of(context).pushNamed(EditTask.routeName);
+          },
+          child: Container(
+            padding: EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: provider.isDarkMode()?
+                  MyTheme.darkGreyColor :
+                  MyTheme.whiteColor
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
                 Container(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 20,
-                    vertical: 5
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: MyTheme.primaryColor
-                  ),
-                  child:
-                  Icon(
-                    Icons.check, size: 40,color: MyTheme.whiteColor,),
-
+                  color:  isDone?
+                      MyTheme.limeColor :
+                      MyTheme.primaryColor,
+                  height: 80,
+                  width: 4,
                 ),
-              ),
+                Expanded(
+                    child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                      AppLocalizations.of(context)!.task_title,
+                      style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                        color: isDone?
+                        MyTheme.limeColor
+                          :
+                        MyTheme.primaryColor
+                      ),),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        AppLocalizations.of(context)!.task_details,
+                      style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                          color: provider.isDarkMode()?
+                          MyTheme.whiteColor :
+                          MyTheme.blackColor)),
+                    ),
+                  ],
+                )),
+                InkWell(
+                  onTap: (){
+                    isDone = true;
+                    setState(() {
 
-            ],
+                    });
+                  },
+                  child: isDone?
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('${AppLocalizations.of(context)!.done}!',
+                      style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                          color: MyTheme.limeColor
+                      ),),
+                  )
+                      :
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 5
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: MyTheme.primaryColor
+                    ),
+                    child:
+                    Icon(
+                      Icons.check, size: 40,color: MyTheme.whiteColor,),
+
+                  ),
+                ),
+
+              ],
+            ),
           ),
         ),
       ),
